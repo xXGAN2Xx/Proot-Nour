34bc5dc2a7612c7159c55a21445140a9ef1a5b7a
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2025-03-31 16:50:02 +0200

    Update PteroVM.sh

--- PteroVM.sh
+++ PteroVM.sh
@@ -80,7 +80,51 @@ display_gg() {
   echo -e "${WHITE}___________________________________________________${RESET_COLOR}"
 }
 
+
+# Fun header
+fun_header() {
+  echo -e "${BOLD_RED}  __      __        ______"
+  echo -e "${BOLD_YELLOW}  \\ \\    / /       |  ____|"
+  echo -e "${BOLD_GREEN}   \\ \\  / / __  ___| |__ _ __ ___  ___   ___  ___"
+  echo -e "${BOLD_CYAN}    \\ \\/ / '_ \\/ __|  __| '__/ _ \\/ _ \\ / _ \\/ __|"
+  echo -e "${BOLD_BLUE}     \\  /| |_) \\__ \\ |  | | |  __/  __/|  __/\\__ \\"
+  echo -e "${BOLD_MAGENTA}      \\/ | .__/|___/_|  |_|  \\___|\\___(_)___||___/"
+  echo -e "${BOLD_WHITE}         | |"
+  echo -e "${BOLD_YELLOW}         |_|"
+  echo -e "${BOLD_GREEN}___________________________________________________"
+  echo -e "          ${BOLD_CYAN}-----> Fun System Resources <----${RESET_COLOR}"
+  echo -e "${BOLD_RED}            Powered by ${BOLD_WHITE}NOUR${RESET_COLOR}"
+}
+
+# Fun system resources display
+fun_resources() {
+  # Get CPU info
+  CPU_MODEL=$(grep -m1 'model name' /proc/cpuinfo | cut -d ':' -f 2 | sed 's/^ //g')
+  CPU_CORES=$(grep -c 'processor' /proc/cpuinfo)
+
+  # Get RAM info (in MB)
+  TOTAL_RAM=$(free -m | awk '/Mem:/ {print $2}')
+  USED_RAM=$(free -m | awk '/Mem:/ {print $3}')
+  FREE_RAM=$(free -m | awk '/Mem:/ {print $4}')
+
+  # Get disk usage (in human-readable form)
+  DISK_USAGE=$(df -h / | awk 'NR==2 {print $3 " used out of " $2}')
+
+  # Get system uptime (replacing 'uptime -p' for compatibility)
+  UPTIME=$(awk '{print int($1/3600)" hours, "int($1%3600/60)" minutes"}' /proc/uptime)
+
+  echo -e "${BOLD_MAGENTA} CPU Model -> ${YELLOW}$CPU_MODEL${RESET_COLOR}"
+  echo -e "${BOLD_CYAN} CPU Cores -> ${GREEN}$CPU_CORES${RESET_COLOR}"
+  echo -e "${BOLD_BLUE} RAM Total -> ${RED}$TOTAL_RAM MB${RESET_COLOR}"
+  echo -e "${BOLD_YELLOW} RAM Used -> ${MAGENTA}$USED_RAM MB${RESET_COLOR}"
+  echo -e "${BOLD_GREEN} RAM Free -> ${CYAN}$FREE_RAM MB${RESET_COLOR}"
+  echo -e "${BOLD_RED} Disk Usage -> ${BOLD_WHITE}$DISK_USAGE${RESET_COLOR}"
+  echo -e "${BOLD_YELLOW} Uptime -> ${BOLD_GREEN}$UPTIME${RESET_COLOR}"
+}
+
 clear
+fun_header
+fun_resources
 display_gg
 
 $ROOTFS_DIR/usr/local/bin/proot \
