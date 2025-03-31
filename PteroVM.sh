6f04b149b6bac31d75f796ee4301e75c9af988f8
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2025-03-31 10:27:07 +0200

    Update PteroVM.sh

--- PteroVM.sh
+++ PteroVM.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+
 ROOTFS_DIR=$(pwd)
 export PATH=$PATH:~/.local/usr/bin
 max_retries=50
 timeout=1
+
 ARCH=$(uname -m)
 if [ "$ARCH" = "x86_64" ]; then
   ARCH_ALT=amd64
@@ -20,6 +22,7 @@ else
   printf "Unsupported CPU architecture: ${ARCH}\n"
   exit 1
 fi
+
 if [ ! -e $ROOTFS_DIR/.installed ]; then
   echo "#######################################################################################"
   echo "#"
@@ -28,50 +31,63 @@ if [ ! -e $ROOTFS_DIR/.installed ]; then
   echo "#                           Copyright (C) 2024, RecodeStudios.Cloud"
   echo "#"
   echo "#######################################################################################"
+
   install_ubuntu=YES
 fi
+
 case $install_ubuntu in
   [yY][eE][sS])
     wget --tries=$max_retries --timeout=$timeout --no-hsts -O /tmp/rootfs.tar.gz \
-       "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64-azure.vhd.tar.gz"
-     tar -xf /tmp/rootfs.tar.gz -C $ROOTFS_DIR
+      "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64-azure.vhd.tar.gz"
+    tar -xf /tmp/rootfs.tar.gz -C $ROOTFS_DIR
     ;;
   *)
     echo "Skipping Ubuntu installation."
     ;;
 esac
+
 if [ ! -e $ROOTFS_DIR/.installed ]; then
   mkdir $ROOTFS_DIR/usr/local/bin -p
   wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/xXGAN2Xx/proot-nour/refs/heads/main/proot"
+
   while [ ! -s "$ROOTFS_DIR/usr/local/bin/proot" ]; do
     rm $ROOTFS_DIR/usr/local/bin/proot -rf
     wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/xXGAN2Xx/proot-nour/refs/heads/main/proot"
+
     if [ -s "$ROOTFS_DIR/usr/local/bin/proot" ]; then
       chmod +x $ROOTFS_DIR/usr/local/bin/proot
       break
     fi
+
     chmod +x $ROOTFS_DIR/usr/local/bin/proot
     sleep 1
   done
+
   chmod +x $ROOTFS_DIR/usr/local/bin/proot
 fi
+
 if [ ! -e $ROOTFS_DIR/.installed ]; then
   printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" > ${ROOTFS_DIR}/etc/resolv.conf
   rm -rf /tmp/rootfs.tar.xz /tmp/sbin
   touch $ROOTFS_DIR/.installed
 fi
+
 CYAN='\e[0;36m'
 WHITE='\e[0;37m'
+
 RESET_COLOR='\e[0m'
-displaygg() {
-  echo -e "${WHITE}__${RESET_COLOR}"
+
+display_gg() {
+  echo -e "${WHITE}___________________________________________________${RESET_COLOR}"
   echo -e ""
-  echo -e "           ${CYAN}-----> Mission Completed ! <----${RESETCOLOR}"
+  echo -e "           ${CYAN}-----> Mission Completed ! <----${RESET_COLOR}"
   echo -e ""
-  echo -e "${WHITE}__${RESET_COLOR}"
+  echo -e "${WHITE}___________________________________________________${RESET_COLOR}"
 }
+
 clear
 display_gg
+
 $ROOTFS_DIR/usr/local/bin/proot \
   --rootfs="${ROOTFS_DIR}" \
   -0 -w "/root" -b /dev -b /sys -b /proc -b /etc/resolv.conf --kill-on-exit
