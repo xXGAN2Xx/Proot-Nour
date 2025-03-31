34f2f1d086dfa465ff3d6b4765001e1cee374059
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2025-03-31 03:02:50 +0200

    Update PteroVM.sh

--- PteroVM.sh
+++ PteroVM.sh
@@ -38,7 +38,7 @@ fi
 case $install_ubuntu in
   [yY][eE][sS])
     wget --tries=$max_retries --timeout=$timeout --no-hsts -O /tmp/rootfs.tar.gz \
-      "http://cdimage.ubuntu.com/ubuntu-base/releases/22.04.5/release/ubuntu-base-22.04.5-base-${ARCH_ALT}.tar.gz"
+      "https://partner-images.canonical.com/core/jammy/current/ubuntu-jammy-core-cloudimg-${ARCH_ALT}-root.tar.gz"
     tar -xf /tmp/rootfs.tar.gz -C $ROOTFS_DIR
     ;;
   *)
