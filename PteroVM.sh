d2ed0e1b29c7c68d40baeff5e6a625c8e9a6d4d6
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2025-03-31 12:15:54 +0200

    Update PteroVM.sh

--- PteroVM.sh
+++ PteroVM.sh
@@ -30,7 +30,7 @@ fi
 
 case $install_ubuntu in
   [yY][eE][sS])
-    curl -sSLo rootfs.tar.gz https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64-azure.vhd.tar.gz >/dev/null 2>err.log
+    curl -sSLo rootfs.tar.gz https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.tar.gz >/dev/null 2>err.log
     tar -xf rootfs.tar.gz >/dev/null 2>err.log
     ;;
   *)
