6091b3fd2415f9f5254f5db5c0bf3b2f334120e4
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2025-04-05 00:38:15 +0200

    Update PteroVM.sh

--- PteroVM.sh
+++ PteroVM.sh
@@ -44,7 +44,7 @@ case $input in
     0)
     wget --tries=$max_retries --timeout=$timeout -O /tmp/rootfs.tar.xz \
     "https://github.com/termux/proot-distro/releases/download/v4.18.0/ubuntu-noble-${ARCH}-pd-v4.18.0.tar.xz"
-    curl -O https://archive.ubuntu.com/ubuntu/pool/main/x/xz-utils/xz-utils_5.6.1+really5.4.5-1ubuntu0.2_amd64.deb
+    curl -O https://archive.ubuntu.com/ubuntu/pool/main/x/xz-utils/xz-utils_5.6.1+really5.4.5-1ubuntu0.2_${ARCH_ALT}.deb
     deb_file=$(ls xz-utils_*.deb)
     dpkg -x "$deb_file" ~/.local/
     rm "$deb_file"
@@ -54,7 +54,7 @@ case $input in
     1)
     wget --tries=$max_retries --timeout=$timeout -O /tmp/rootfs.tar.xz \
     "https://github.com/termux/proot-distro/releases/download/v4.21.0/alpine-${ARCH}-pd-v4.21.0.tar.xz"
-    curl -O https://archive.ubuntu.com/ubuntu/pool/main/x/xz-utils/xz-utils_5.6.1+really5.4.5-1ubuntu0.2_amd64.deb
+    curl -O https://archive.ubuntu.com/ubuntu/pool/main/x/xz-utils/xz-utils_5.6.1+really5.4.5-1ubuntu0.2_${ARCH_ALT}.deb
     deb_file=$(ls xz-utils_*.deb)
     dpkg -x "$deb_file" ~/.local/
     rm "$deb_file"
