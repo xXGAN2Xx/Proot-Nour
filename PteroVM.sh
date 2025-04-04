d4512a6e2ed4ca369a97cd66bd4b2c38df6378ab
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2025-04-04 23:42:23 +0200

    Update PteroVM.sh

--- PteroVM.sh
+++ PteroVM.sh
@@ -174,4 +174,4 @@ display_footer
 
 # This command starts PRoot and binds several important directories
 # from the host file system to our special root file system.
-$ROOTFS_DIR/usr/local/bin/proot -S "${ROOTFS_DIR}" -w "/root" -b /etc/resolv.conf /bin/bash --kill-on-exit
+$ROOTFS_DIR/usr/local/bin/proot -S "${ROOTFS_DIR}" -w "/root" -b /etc/resolv.conf /bin/sh --kill-on-exit
