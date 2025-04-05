d0038e9b7a3c1b2859226a72412b93c7d1cf6e8b
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2025-04-05 18:25:12 +0200

    Update PteroVM.sh

--- PteroVM.sh
+++ PteroVM.sh
@@ -1,34 +1,9 @@
 #!/bin/sh
-
-# Define the root directory to /home/container.
-# We can only write in /home/container and /tmp in the container.
 ROOTFS_DIR=/home/container
-
 export PATH=$PATH:~/.local/usr/bin
-
-
-max_retries=50
-timeout=3
-
-
-# Detect the machine architecture.
 ARCH=$(uname -m)
-
-# Check machine architecture to make sure it is supported.
-# If not, we exit with a non-zero status code.
-if [ "$ARCH" = "x86_64" ]; then
-  ARCH_ALT="amd64"
-elif [ "$ARCH" = "aarch64" ]; then
-  ARCH_ALT="arm64"
-else
-  printf "Unsupported CPU architecture: ${ARCH}"
-  exit 1
-fi
-
-    curl -O https://archive.ubuntu.com/ubuntu/pool/main/x/xz-utils/xz-utils_5.6.1+really5.4.5-1ubuntu0.2_${ARCH_ALT}.deb
-    deb_file=$(ls xz-utils_*.deb)
-    dpkg -x "$deb_file" ~/.local/
-    rm "$deb_file"
-    export PATH=~/.local/usr/bin:$PATH
-
-    sh "/install.sh"
+mkdir -p ${ROOTFS_DIR}/usr/local/bin
+proot_url="https://github.com/ysdragon/proot-static/releases/download/v5.4.0/proot-${ARCH}-static"
+curl -Ls "$proot_url" -o ${ROOTFS_DIR}/usr/local/bin/proot
+chmod +x ${ROOTFS_DIR}/usr/local/bin/proot
+exec ./entrypoint.sh
