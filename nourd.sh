a36887234ac0ab4e9c7dd7f5207492ae10a4b2ce
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2025-06-22 13:35:46 +0300

    Update nourd.sh

--- nourd.sh
+++ nourd.sh
@@ -257,4 +257,4 @@ display_footer
 
 # This command starts PRoot and binds several important directories
 # from the host file system to our special root file system.
-$ROOTFS_DIR/usr/local/bin/proot --rootfs="${ROOTFS_DIR}" -0 -w "/root" -b /dev -b /sys -b /proc -b /etc/resolv.conf -l --kill-on-exit
+$ROOTFS_DIR/usr/local/bin/proot --rootfs="${ROOTFS_DIR}" -0 -l -w "/root" -b /dev -b /sys -b /proc -b /etc/resolv.conf --kill-on-exit
