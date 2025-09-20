6c52bed90aa12c5dd168915df34dc5f2c17ca470
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2025-09-21 01:19:05 +0300

    Update nourd.sh

--- nourd.sh
+++ nourd.sh
@@ -236,7 +236,7 @@ cleanup() {
 trap 'cleanup' INT TERM
 "$ROOTFS_DIR/usr/local/bin/proot" --rootfs="${ROOTFS_DIR}" -0 -w "/root" \
     -b /dev -b /sys -b /proc -b /etc/resolv.conf --kill-on-exit \
-    /bin/bash -c "/root/startup.sh && tmate -F" &
+    /bin/bash -c "/root/startup.sh && tmate" &
 PROOT_PID=$!
 echo "INFO: PRoot environment started with PID: $PROOT_PID"
 wait $PROOT_PID
