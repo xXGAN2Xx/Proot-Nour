c7f2e89dd3a3c74e32bce5ff36a94cf318226809
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2025-09-21 00:01:54 +0300

    Update nourd.sh

--- nourd.sh
+++ nourd.sh
@@ -158,7 +158,7 @@ display_resources() {
     echo -e " CPU -> ${YELLOW}${cpu_model}${RESET}"
     echo -e " RAM -> ${GREEN}${SERVER_MEMORY:-N/A}MB${RESET}"
     echo -e " PRIMARY PORT -> ${GREEN}${SERVER_PORT}${RESET}"
-    echo -e " PRIMARY IP   -> ${GREEN}${SERVER_ALLOCATION}${RESET}"
+    echo -e " PRIMARY IP   -> ${GREEN}printenv${RESET}"
 }
 
 display_footer() {
