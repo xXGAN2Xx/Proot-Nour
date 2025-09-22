e40849307273a05161d759d91d6d69c0f9ea704f
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2025-09-22 21:09:58 +0300

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -67,8 +67,8 @@ else
 fi
 
 echo "--- Starting sing-box service... ---"
+echo "sing-box started in the background."
 echo "vless://bf000d23-0752-40b4-affe-68f7707a9661@${PUBLIC_IP}:${SERVER_PORT}?encryption=none&security=tls&sni=playstation.net&alpn=h3&allowInsecure=1&type=tcp&headerType=none#nour-vless"
 systemctl enable sing-box
 systemctl start sing-box
 #sing-box run --config /etc/sing-box/config.json &
-echo "sing-box started in the background."
