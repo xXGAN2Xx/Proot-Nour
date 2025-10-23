725864ca3e463b96e26c3d8d8607c06ced4b4a7d
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2025-10-23 18:34:29 +0300

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -9,7 +9,7 @@ mkdir -p /etc/sing-box
 if [ ! -f "$INSTALL_LOCK_FILE" ]; then
     echo "First time setup: Updating package lists and installing dependencies..."
     apt-get update > /dev/null 2>&1
-    apt-get install -y curl git gpg sudo wget tmate screen bash nano python3-minimal > /dev/null 2>&1
+    apt-get install -y curl git gpg sudo wget tmate screen bash nano dos2unix python3-minimal > /dev/null 2>&1
 
     echo "Installing sing-box for the first time..."
     curl -fsSL https://sing-box.app/install.sh | sh
