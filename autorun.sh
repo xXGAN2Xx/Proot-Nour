c4289a18c85f3c8f619a2d31d901b0ca8b943d6a
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2025-12-17 16:44:35 +0200

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -68,7 +68,7 @@ fi
 # B. Xray-core Installation/Update (RUNS EVERY TIME)
 echo "Checking for Xray-core updates and installing..."
 # This command will update Xray if a new version is available, or reinstall it.
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --without-geodata
 
 # --- STEP 3: Download config.json and Configure ---
 echo "Downloading latest config.json..."
