fb87758031650d0f3c99f5ef82fcf88a5035fcac
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2026-03-15 14:03:36 +0200

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -11,7 +11,7 @@ DEP_LOCK_FILE="/etc/os_deps_installed"
 if [ ! -f "$DEP_LOCK_FILE" ]; then
     echo "--- [1] Installing Dependencies ---"
     apt-get update -y
-    apt-get install -y curl wget openssl
+    apt-get install -y curl wget openssl ca-certificates 
     touch "$DEP_LOCK_FILE"
 else
     echo "--- [1] Dependencies already installed. Skipping. ---"
