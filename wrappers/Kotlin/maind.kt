ff0ffbe64c62511b1d15813b16405d26a913d96d
IINourYT <38559520+xXGAN2Xx@users.noreply.github.com>
2026-04-30 20:48:50 +0300

    Update maind.kt

--- wrappers/Kotlin/maind.kt
+++ wrappers/Kotlin/maind.kt
@@ -184,7 +184,7 @@ fun runScript(scriptFile: File) {
         writer.newLine()
         writer.flush()
 
-        writer.write("1")
+        writer.write("4")
         writer.newLine()
         writer.flush()
 
